ec9ab4ed9b6abea8929b936137c9589e3021143d
DieRandomDie <EricRandom1023@gmail.com>
2022-02-18 11:29:37 -0600

    made some progress on the basic layout and began some script stuff.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+apikey = document.getElementsByClassName("api-key")[0]
+console.log(apikey.value)
+
+var apikey = document.getElementsByClassName("api-key")[0];
+apikey.addEventListener("keydown", function(event) {
+    if ((event.keyCode === 9 || 13) /*&& apikey.value != ''*/) {
+        if (event.keyCode === 13) {
+            document.getElementsByClassName("blacksmith")[0].focus();
+        }
+        console.log("event");
+    }
+});
