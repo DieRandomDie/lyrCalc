5b9edc87c085f944ad614abc45047656e31d32ba
DieRandomDie <EricRandom1023@gmail.com>
2022-02-21 22:11:40 -0600

    api fetch added. just need to add calcs then work on the other planned features like keeping weaps/arms the same with a toggle. etc

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 apikey = document.getElementsByClassName("api-key")[0]
+current = document.getElementsByClassName('current')[0].children
+goal = document.getElementsByClassName('goal')[0].children
 console.log(apikey.value)
+let equips
 
 var apikey = document.getElementsByClassName("api-key")[0];
 apikey.addEventListener("keydown", function(event) {
-    if ((event.keyCode === 9 || 13) /*&& apikey.value != ''*/) {
+    if ((event.keyCode === 9 || event.keyCode === 13) /*&& apikey.value != ''*/) {
         if (event.keyCode === 13) {
             event.preventDefault()
             document.getElementsByClassName("blacksmith")[0].focus();
         }
-        console.log("event");
+        equips = fetchAPI(apikey.value)
     }
+
 });
+
+function fetchAPI(api_key) {
+    let equipment = {"orbs":[],"levels":[]}
+
+    fetch('https://lyrania.co.uk/api/accounts.php?search='+api_key)
+        .then(res => {
+            if (res.ok) {
+                console.log("FETCH RETURNED CODE: "+res.status)
+                res.json()
+                    .then(data => {
+                        let x = 0
+                        let replace = document.createElement('div')
+                        for (const [key, value] of Object.entries(data.equipment)) {
+                            current[x].value = value
+                            replace.innerHTML = current[x].value
+                            goal[x].value = value
+                            x++
+                            //equipment.orbs.push(key.split(" ")[0])
+                            //equipment.levels.push(value)
+                        }
+
+                    })
+            } else {
+                console.log("FETCH FAILED. ERROR:" + res.status)
+            }
+        })
+    return equipment
+}
