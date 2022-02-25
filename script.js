5aaa3d9938fe5cfd411eae2683bb871b2829a029
DieRandomDie <EricRandom1023@gmail.com>
2022-02-24 21:13:25 -0600

    toggle is now complete. total also updates as expected I think

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 let apikey = document.getElementById("api-key")
+let toggle = document.getElementById("toggle-ratio")
 let current = document.getElementsByClassName('current-equip-class')
 const currentInputs = document.querySelectorAll('input.current-equip-class')
 let goal = document.getElementsByClassName('goal-equip-class')
 const goalInputs = document.querySelectorAll('input.goal-equip-class')
+const sameGoals = document.querySelectorAll('input.same-goal-equip-class')
 const costOutputs = document.querySelectorAll('output.cost-equip-class')
 console.log(apikey.value)
 let equipment = {}
 
 
 function toggleRatio() {
+    if(toggle.checked) {
+        sameGoals.forEach(goal => {
+            goal.disabled = true
+            goal.style.visibility = "hidden"
+        })
+    } else {
+        goalInputs.forEach(goal => {
+            goal.disabled = false
+            goal.style.visibility = "visible"
+        })
+    }
+}
+
+
+function toggleRatioUpdate() {
+    if(toggle.checked) {
+        goalInputs[1].value = goalInputs[0].value
+        goalInputs[3].value = goalInputs[2].value
+        goalInputs[4].value = goalInputs[2].value
+        goalInputs[5].value = goalInputs[2].value
+        goalInputs[6].value = goalInputs[2].value
+        goalInputs[7].value = goalInputs[2].value
+        goalInputs[8].value = goalInputs[2].value
+        updateAllGoals()
+    }
 }
 
 function updateAllGoals() {
@@ -29,30 +56,40 @@ function fetchAPI(api_key) {
         updateAllGoals()
         return
     }
-
-    fetch('https://lyrania.co.uk/api/accounts.php?search=' + api_key)
-        .then(res => {
-            if (res.ok) {
-                console.log("FETCH RETURNED CODE: " + res.status)
-                res.json()
-                    .then(data => {
-                        console.log(data.equipment)
-                        let x = 0
-                        for (const [key, value] of Object.entries(data.equipment)) {
-                            current[x].value = value
-                            goal[x].value = value
-                            x++
-                            equipment[key.split(" ")[1]] = {
-                                orb: key.split(" ")[0],
-                                level: Number(value)
+    if (api_key.length === 32) {
+        fetch('https://lyrania.co.uk/api/accounts.php?search=' + api_key)
+            .then(res => {
+                if (res.ok) {
+                    console.log("FETCH RETURNED CODE: " + res.status)
+                    res.json()
+                        .then(data => {
+                            console.log(data.equipment)
+                            let x = 0
+                            for (const [key, value] of Object.entries(data.equipment)) {
+                                current[x].value = value
+                                goal[x].value = value
+                                x++
+                                equipment[key.split(" ")[1]] = {
+                                    orb: key.split(" ")[0],
+                                    level: Number(value)
+                                }
                             }
-                        }
-                        currentInputs.forEach(input => input.disabled = true)
-                    })
-            } else {
-                console.log("FETCH FAILED. ERROR:" + res.status)
-            }
-        })
+                            currentInputs.forEach(input => input.disabled = true)
+                            updateAllGoals()
+                        })
+                } else {
+                    console.log("FETCH FAILED. ERROR:" + res.status)
+                }
+            })
+    } else {
+        apikey.type = ""
+        apikey.value = "INVALID"
+        setTimeout(() => {
+            apikey.type = "password"
+            apikey.value = ""
+            apikey.focus(apikey.select())
+        }, 1000)
+    }
 }
 
 
@@ -77,6 +114,19 @@ function ecalc(equip_name) {
         cost += ((0.005 * (i ** 2)) - .0101 * i + .0052) * discountValue
     }
     result.value = Math.ceil(cost).toLocaleString() + "p"
+    totalCost()
+}
+
+
+function totalCost() {
+    let total = document.getElementById("total-cost")
+    let totalValue = 0
+    let rval = 0
+    costOutputs.forEach(result => {
+        rval = Number(result.innerHTML.replace(/,|p/g, ''))
+        totalValue += rval
+    })
+    total.innerHTML = totalValue.toLocaleString() + "p"
 }
 
 
@@ -98,3 +148,6 @@ window.onload = function() {
             }
         })
 }
+
+
+let power = ((0.5*level*(level-1)+1)+((0.5*level*(level-1)+1)*orbPercent))*(1+enchantPercent)*(1+festPercent)
